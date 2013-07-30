e4ff90373fb7bb4b8fbf43685c41404f565442e4
Tomasz Rzedowski <neblina.project@o2.pl>
2013-07-30 16:19:50 -0400

    vm structure

--- vm/vm.h
+++ vm/vm.h
@@ -6,45 +6,24 @@
 using namespace std;
 
 
-
-class State {
-    int value;
-};
-
-class Name {
-    string value;
-};
-
-class Code {
-    
-};
-
-class Message {
-    int value;
-};
+class Code {};
 
 class Cell {
-    public:
-	Cell *out, *ina,*inb;
-	queue<Message> qout,qina,qinb;
-	State state;
-	Name name;
+	int id;
+	int state;
 	Code code;
-	
-	void set_name(Name n);
-	void set_state(State s);
-	
-	void connect_out_to_ina(Cell* cell);
-	void connect_out_to_inb(Cell* cell);
-	void connect_ina_to_out(Cell* cell);
-	void connect_inb_to_out(Cell* cell);
-	
-	void run_single_instruction();
 };
 
-class Bucket {};
+class Bucket {
+    queue<Cell*> list;
+};
 
-
+class Packet {
+    int from,to;
+    int toin;
+    int fromout;
+    int message;
+};
 
 
 #endif
