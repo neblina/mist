d98b1c2710004d8520201c55f6fe507d5e919dd6
Tomasz Rzedowski <neblina.project@o2.pl>
2013-07-30 15:56:54 -0400

    Declaration on cell class

--- vm/vm.h
+++ vm/vm.h
@@ -1,29 +1,48 @@
-#ifndef _VM_H_
-#define _VM_H_
+#ifndef _vmh_
+#define _vmh_
 
-#include "types.h"
+#include <iostream>
+#include <queue>
+using namespace std;
 
-class MCell {
-    public:
-	MCell (){}
-	~MCell (){}
 
-	void set_id(MCellId lid);
-	MCellId get_id();
 
-	void set_state(MCellState lstate);
-	MCellState get_state();
-	
-
-    private:
-	MCellId id;
-	MCell* output;
-	MCell* inputA, *inputB;
-	MCellState state;
-	
+class State {
+    int value;
 };
 
+class Name {
+    string value;
+};
 
+class Code {
+    
+};
+
+class Message {
+    int value;
+};
+
+class Cell {
+    public:
+	Cell *out, *ina,*inb;
+	queue<Message> qout,qina,qinb;
+	State state;
+	Name name;
+	Code code;
+	
+	void set_name(Name n);
+	void set_state(State s);
+	
+	void connect_out_to_ina(Cell* cell);
+	void connect_out_to_inb(Cell* cell);
+	void connect_ina_to_out(Cell* cell);
+	void connect_inb_to_out(Cell* cell);
+	
+	void run_single_instruction();
+};
+
+class Bucket {};
 
 
 
